6187100dc0134757c7c5d1e51d1aefb5a0c074b1
Trang Nguyen <trang.nguyen@benevity.com>
2023-11-06 11:36:24 -0600

    modify Path class

--- src/main/Path.java
+++ src/main/Path.java
@@ -1,17 +1,19 @@
 package main;
 
-import java.util.Iterator;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Path {
-    private LinkedList<Integer> path;
+    private final List<Integer> path;
 
     // For testing purposes
     public Path(int ... vertices) {
-        path = new LinkedList<>();
-        for (int v : vertices) {
-            path.add(v);
-        }
+        path = Arrays.stream(vertices)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toList(),  Collections::unmodifiableList));
     }
 
     public Path() {
@@ -22,8 +24,12 @@ public class Path {
         path.add(v);
     }
 
-    Iterator<Integer> getIterator() {
-        return path.iterator();
+    public int getSize() {
+        return path.size();
+    }
+
+    public List<Integer> getPath() {
+        return path;
     }
 
     @Override
@@ -31,5 +37,9 @@ public class Path {
         return "Path{" +
                 "path=" + path +
                 '}';
+    }
+
+    int getVertex(final int i) {
+        return path.get(i);
     }
 }
