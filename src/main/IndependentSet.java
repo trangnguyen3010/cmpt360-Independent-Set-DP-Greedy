ba27495221ef2210dead5e525bece5f1ea36a026
Trang Nguyen <trang.nguyen@benevity.com>
2023-11-10 11:36:05 -0600

    update functions for printing paths

--- src/main/IndependentSet.java
+++ src/main/IndependentSet.java
@@ -14,6 +14,40 @@ public class IndependentSet {
         return memoi[path.getSize() + 1];
     }
 
+    public static String findIndependentSetMaxWeightPath(Path path) {
+        int[] memoi = new int[path.getSize() + 2];
+        memoi[0] = 0;
+        memoi[1] = 0;
+        StringBuilder onePreviousSet = new StringBuilder();
+        StringBuilder twoPreviousSet = new StringBuilder();
+        for (int i = 2; i < path.getSize() + 2; i++) {
+            int currentVertex = path.getVertex(i - 2);
+            memoi[i] = Math.max(memoi[i - 1], memoi[i - 2] + currentVertex);
+
+            if (memoi[i - 1] > memoi[i - 2] + currentVertex) {
+                onePreviousSet.append(".").append(" ");
+                twoPreviousSet = new StringBuilder().append(onePreviousSet);
+            } else {
+                twoPreviousSet.append(currentVertex).append(" ");
+                onePreviousSet.append(".").append(" ");
+            }
+
+            StringBuilder temp = onePreviousSet;
+            onePreviousSet = twoPreviousSet;
+            twoPreviousSet = temp;
+        }
+
+        if (onePreviousSet.substring(onePreviousSet.length() - 2).equals(". ")) {
+            twoPreviousSet.append("= ");
+            twoPreviousSet.append(memoi[path.getSize() + 1]);
+            return twoPreviousSet.toString();
+        } else {
+            onePreviousSet.append("= ");
+            onePreviousSet.append(memoi[path.getSize() + 1]);
+            return onePreviousSet.toString();
+        }
+    }
+
     public static void main(String[] args) {
         // Write code to read the standard input
         // and print the output.
@@ -29,12 +63,12 @@ public class IndependentSet {
                 scanner.nextLine();
             }
         }
-        System.out.println(findIndependentSetMaxWeight(path));
+        System.out.println(findIndependentSetMaxWeightPath(path));
     }
 
     private static int getNextInt(final Scanner scanner) {
         if (scanner.hasNextInt()) {
-            int number =  scanner.nextInt();
+            int number = scanner.nextInt();
             verifyBiggerThanZero(number);
             return number;
         } else {
